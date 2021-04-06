249c19402be203e3d1ef1000b40bec2b4968efaf
Haider Ali Punjabi <haiderali@cyberservices.com>
2021-04-06 17:21:55 +0530

    Fixed Design

--- components/sections/footer.js
+++ components/sections/footer.js
@@ -2,23 +2,23 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function Footer() {
   return (
-    <footer className="text-center pt-16 pb-8 text-l sm:text-2xl md:text-3xl">
-      <div className="flex flex-col container mx-auto">
-        <div className="flex justify-around">
-          <div className="w-2/5">
-            <img className="w-32 mx-auto" src="/logo.svg" />
-          </div>
-          <div className="flex justify-around flex-wrap gap-3 sm:gap-5 md:gap-7 lg:10 my-auto">
-            <div>
+    <footer className="text-center pt-16 pb-8 text-l sm:text-xl md:text-3xl">
+      <div className="flex container mx-auto justify-around mb-10">
+        <div className="w-1/5">
+          <img className="w-40 mx-auto" src="/logo.svg" />
+        </div>
+        <div className="w-3/5 flex flex-col w-auto space-y-10">
+          <div className="flex justify-between flex-wrap">
+            <div className="mr-2">
               <a href="/">Home</a>
             </div>
-            <div>
+            <div className="mr-2">
               <a href="#photos">Photos</a>
             </div>
-            <div>
+            <div className="mr-2">
               <a href="#about">About</a>
             </div>
-            <div>
+            <div className="mr-2">
               <a
                 href="https://photos.app.goo.gl/2W6GyJDomNyhywXM8"
                 target="_blank"
@@ -28,20 +28,20 @@ export default function Footer() {
               </a>
             </div>
           </div>
-          </div>
-          <div>
-            Stay Connected at
+          <div className="flex self-center items-center">
+            <span>Stay Connected at</span>
             <a
-              className="noanimation inline-block text-l sm:text-xl  md:text-2xl border border-black p-4 mb-10 mx-4 mt-4 hover:shadow-lg"
+              className="noanimation inline-block text-l sm:text-xl md:text-2xl border border-black p-2 mx-4 hover:shadow-lg text-center"
               href="https://instagram.com/kashmirtypehunt"
               target="_blank"
               rel="noopener noreferrer"
             >
-              <FontAwesomeIcon className="mx-4" icon={["fab", "instagram"]} />
+              <FontAwesomeIcon className="mr-2" icon={["fab", "instagram"]} />
               kashmirtypehunt
             </a>
           </div>
         </div>
+      </div>
       <div>
         <h3>
           Made with <span className="text-red-500">&hearts;</span> by{" "}
