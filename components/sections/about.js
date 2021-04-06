1c6a9cc2bbf2c56b4606755121a1c91bc9e9f678
Haider Ali Punjabi <haiderali@cyberservices.com>
2021-04-06 14:33:56 +0530

    Design additions

--- components/sections/about.js
+++ components/sections/about.js
@@ -1,12 +1,12 @@
 export default function About() {
     return (
-        <div id="about" className="text-center py-16">
+        <div id="about" className="py-16">
             <div className="container mx-auto ">
-                <h1 className="text-4xl sm:text-5xl md:text-6xl font-bold font-mono	font-mono py-7">
-                    About
+                <h1 className="text-center text-4xl sm:text-5xl md:text-6xl font-bold font-mono	font-mono py-7">
+                    About Us
                 </h1>
                 <h2 className="text-2xl sm:text-3xl md:text-4xl font-light">
-                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eos tempora amet cumque ullam veritatis, in ipsam est a, nulla explicabo beatae sunt dolorem. Facilis, nisi dolore voluptatibus voluptates minima ex.
+                    Kashmir Type Hunt is a design community based in Kashmir. Our motive is to document and archive indigenous type forms. The goal is to build awareness about design and type forms around us. What started as individuals documenting type has now evolved into a community where everyone can submit their find thus documenting and showcasing for all the people out there.
                 </h2>
             </div>
         </div>
--- components/sections/footer.js
+++ components/sections/footer.js
@@ -3,35 +3,29 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 export default function Footer() {
   return (
     <footer className="text-center pt-16 pb-8 text-xl sm:text-2xl md:text-3xl">
-      <div className="flex flex-wrap justify-around gap-10">
-        <div>
-          <img className="w-32" src="/logo.svg" />
+      <div className="flex container mx-auto justify-around">
+        <div className="w-2/5">
+          <img className="w-32 mx-auto" src="/logo.svg" />
         </div>
-        <div>
-          <h3>Follow Us</h3>
-          <ul className="flex justify-around my-4">
-            {/* <li><a href="http://" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={["fab","twitter"]} /></a></li> */}
-            <li>
-              <a
-                href="https://www.instagram.com/kashmirtypehunt/"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <FontAwesomeIcon icon={["fab", "instagram"]} />
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div>
-          If you would like to contribute <br />
+        <div className="w-3/5 flex flex-col w-auto">
+          <div className="flex justify-around">
+            <div><a href="/">Home</a></div>
+            <div><a href="#photos">Photos</a></div>
+            <div><a href="#about">About</a></div>
+            <div><a href="https://photos.app.goo.gl/2W6GyJDomNyhywXM8" target="_blank" rel="noopener noreferrer">Submit</a></div>
+          </div>
+          <div>
+          Stay Connected at 
           <a
-            className="noanimation inline-block text-2xl border border-black p-4 mb-10  mt-4 hover:shadow-lg"
-            href="https://photos.app.goo.gl/2W6GyJDomNyhywXM8"
+            className="noanimation inline-block text-2xl border border-black p-4 mb-10 mx-4 mt-4 hover:shadow-lg"
+            href="https://instagram.com/kashmirtypehunt"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Submit a Photo
+            <FontAwesomeIcon className="mx-4" icon={["fab","instagram"]}/>
+            kashmirtypehunt
           </a>
+          </div>
         </div>
       </div>
       <div>
