6cb565e8b780ea4373a97f3e82f9a899f6d10842
Haider Ali Punjabi <haiderali@cyberservices.com>
2021-04-07 07:26:05 +0530

    Added Source Maps

--- next.config.js
+++ next.config.js
@@ -22,5 +22,6 @@ module.exports = withPlugins([
     },
     images: {
       imageSizes: [16, 32, 48, 64, 96, 128, 256, 384,400,512,800,1024],
-    }
+    },
+    productionBrowserSourceMaps: true,
 });
